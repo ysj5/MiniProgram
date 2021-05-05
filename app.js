bef6b1ba763cda41f908a17ca7d4b68df222eb9e
ysj <584983450@qq.com>
2021-05-05 10:02:31 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 App({
- 
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function (options) { //初始化时执行一次
+    //发送网络请求
+    //获取用户信息
+    console.log("小程序初始化完成：onLaunch");
+    console.log(options.scene);
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) { //每次进入都执行一次
+    console.log("界面显示出来： onShow");
+    //判断小程序的进入场景
+    console.log(options.scene);
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log("界面被隐藏时会执行：onHide");
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+  globalData: { //存放全局数据
+    name: "ysj",
+    age: 18
+  }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
 // pages/home/home.js
+//拿到全局数据
+const app = getApp(); //getApp()获取App产生的示例对象
+const name = app.globalData.name;
+console.log(name);
 Page({
-
- /**
-  * 页面的初始数据
-  */
- data: {
-
- },
-
- /**
-  * 生命周期函数--监听页面加载
-  */
- onLoad: function (options) {
-
- },
-
- /**
-  * 生命周期函数--监听页面初次渲染完成
-  */
- onReady: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面显示
-  */
- onShow: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面隐藏
-  */
- onHide: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面卸载
-  */
- onUnload: function () {
-
- },
-
- /**
-  * 页面相关事件处理函数--监听用户下拉动作
-  */
- onPullDownRefresh: function () {
-
- },
-
- /**
-  * 页面上拉触底事件的处理函数
-  */
- onReachBottom: function () {
-
- },
-
- /**
-  * 用户点击右上角分享
-  */
- onShareAppMessage: function () {
-
- }
+	data: {
+		name: "Coderwhy",
+		age: 18
+	},
+	onLoad: function() {
+	},
+	getUserProfile() {
+		// 获取用户信息
+		wx.getUserProfile({
+				desc: '用于完善会员资料',
+				success: (res) => {
+						console.log(res.userInfo);
+				}
+		})
+}
 })
