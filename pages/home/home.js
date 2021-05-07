c6f756a7f425f3f7bda081919f3eb4c3abfbf6cb
ysj <584983450@qq.com>
2021-05-07 15:37:20 +0800

    事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,32 @@
 // pages/home/home.js
 Page({
-
- /**
-  * 页面的初始数据
-  */
- data: {
-
- },
-
- /**
-  * 生命周期函数--监听页面加载
-  */
- onLoad: function (options) {
-
- },
-
- /**
-  * 生命周期函数--监听页面初次渲染完成
-  */
- onReady: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面显示
-  */
- onShow: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面隐藏
-  */
- onHide: function () {
-
- },
-
- /**
-  * 生命周期函数--监听页面卸载
-  */
- onUnload: function () {
-
- },
-
- /**
-  * 页面相关事件处理函数--监听用户下拉动作
-  */
- onPullDownRefresh: function () {
-
- },
-
- /**
-  * 页面上拉触底事件的处理函数
-  */
- onReachBottom: function () {
-
- },
-
- /**
-  * 用户点击右上角分享
-  */
- onShareAppMessage: function () {
-
- }
+  data: {
+    titles: ['衣服', '裤子', '鞋子', '帽子']
+  },
+  handleTap() {
+    console.log("按钮发生了点击")
+  },
+  handleItemClick(event) {
+    console.log(event);
+    console.log(event.currentTarget.dataset);
+  },
+  //事件冒泡和捕获
+  handleCaptureview1(event) { //事件捕获1
+    console.log("事件捕获1");
+  },
+  handleBindView1(event) { //事件冒泡1
+    console.log("事件冒泡1");
+  },
+  handleCaptureview2(event) { //事件捕获2
+    console.log("事件捕获2");
+  },
+  handleBindView2(event) { //事件冒泡2
+    console.log("事件冒泡2");
+  },
+  handleCaptureview3(event) { //事件捕获3
+    console.log("事件捕获3");
+  },
+  handleBindView3(event) { //事件冒泡3
+    console.log("事件冒泡3");
+  }
 })
