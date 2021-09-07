1715ee3605c2d8f583756d786fc1c1270bd6d2b6
您的老祖宗 <584983450@qq.com>
2021-09-07 21:38:48 +0800

    网络请求封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,4 @@
+import request from "../../network/network"
 // pages/home/home.js
 Page({
 
@@ -11,10 +12,52 @@ Page({
  /**
   * 生命周期函数--监听页面加载
   */
- onLoad: function (options) {
+ onLoad: function (options) { //请求网络数据
+  request({
+    url: "/post",
+    method: "post"
+  }).then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(err);
+  })
+  // 原生发送网络请求
+  // wx.request({
+  //   url: 'url',
+  //   methods: "post",
+  //   data: {
+  //     type: "sell"
+  //   },
+  //   success: function(res) { //请求程成功的回调
+  //     console.log(res);
+  //   }
+  // })
 
+  //2.get请求携带参数
+  // wx.request({
+  //   url: 'url',
+  //   data: {
+  //     type: "sell"
+  //   },
+  //   success: function(res) {
+  //     console.log(res);
+  //   }
+  // })
+  //3.post请求携带参数
+  // wx.request({
+  //   url: 'url',
+  //   method: "post",
+  //   data: {
+  //     name: 'ysj'
+  //   },
+  //   success: function(res) {
+  //     console.log(res);
+  //   },
+  //   fail: function (err) {
+  //     console.log("请求出错时调用");
+  //   }
+  // })
  },
-
  /**
   * 生命周期函数--监听页面初次渲染完成
   */
